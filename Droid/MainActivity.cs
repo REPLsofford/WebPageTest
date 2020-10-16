ed68896fd7c53613b1c731cc11ad8d2679f6fffd
Simon Offord <simon.offord@replgroup.com>
2020-10-16 15:29:34 +0100

    Initial commit

---
+++ Droid/MainActivity.cs
@@ -0,0 +1,32 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Webkit;
+
+namespace WebPageTest.Droid
+{
+    [Activity(Label = "WebPageTest", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            var webView = FindViewById<WebView>(Resource.Id.webView);
+            webView.LoadUrl("https://ppe.ourtesco.com/scheduler");
+
+            // Enabled Java Script
+            var settings = webView.Settings;
+            settings.JavaScriptEnabled = true;
+            settings.LoadWithOverviewMode = true;
+            settings.UseWideViewPort = true;
+            webView.SetInitialScale(1);
+        }
+    }
+}
+
---
+++ WebPageTest/MyClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace WebPageTest
+{
+    public class MyClass
+    {
+        public MyClass()
+        {
+        }
+    }
+}
